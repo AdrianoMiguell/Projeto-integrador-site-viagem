69896136bf375ebdccde07f94ce1f01cc9d722ef
AdrianoMiguell <adrianosantos260804@gmail.com>
2023-12-06 09:03:15 -0300

    Alteração pequena

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Nômashe') }}</title>
+    <title> Nômashe </title>
 
     {{-- Bootstrap --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
